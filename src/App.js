3a2719ff307e3e3324aabd3b2d8d585a8eb33f80
Nicolás Gómez <nigomezgu@unal.edu.co>
2021-02-12 16:13:00 -0500

    Progress in the first screen.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
 import React from "react";
-import "./App.css";
 import Backend from "./Backend";
+import { Row, Col, Table, Typography } from "antd";
+
+import "./App.css";
+
+const { Title } = Typography;
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      columns: [
+        { title: "Nombre de usuario", dataIndex: "user", align: "center" },
+        { title: "Detalles", dataIndex: "details", align: "center" },
+      ],
+      data: [],
+    };
+  }
+
   componentDidMount() {
     Backend.SendRequest("users", "GET", {}).then(async (response) => {
-      let res = await response.json();
-      console.log(res);
+      if (response.status === 200) {
+        let res = await response.json();
+        let auxdata = [];
+        res.data.forEach((user) => {
+          auxdata.push({
+            key: user.id,
+            user: user.name,
+          });
+        });
+        this.setState({
+          data: auxdata,
+        });
+      } else {
+        console.error("Error making the request: " + response.status);
+      }
     });
   }
 
   render() {
     return (
-      <div className="App">
-        <p>Hi.</p>
+      <div className="App-Div-Main">
+        <Row justify="center">
+          <Col className="App-Col-Title" xs={22} md={21} lg={20}>
+            <Title className="App-Title">LISTA DE USUARIOS</Title>
+          </Col>
+        </Row>
+        <Row justify="center">
+          <Col xs={22} md={18} lg={12}>
+            <Table
+              bordered
+              columns={this.state.columns}
+              dataSource={this.state.data}
+            ></Table>
+          </Col>
+        </Row>
       </div>
     );
   }
