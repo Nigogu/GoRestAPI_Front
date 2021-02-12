3fd6f9df7f074887e52288fd713b4f4abd1de801
Nicolás Gómez <nigomezgu@unal.edu.co>
2021-02-12 17:17:51 -0500

    Table requests and pagination working properly.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Backend from "./Backend";
-import { Row, Col, Table, Typography } from "antd";
+import { Row, Col, Table, Typography, Button } from "antd";
 
 import "./App.css";
 
@@ -11,31 +11,91 @@ class App extends React.Component {
     super(props);
     this.state = {
       columns: [
-        { title: "Nombre de usuario", dataIndex: "user", align: "center" },
-        { title: "Detalles", dataIndex: "details", align: "center" },
+        {
+          title: (
+            <Title level={4} style={{ marginBottom: 0 }}>
+              Nombre de usuario
+            </Title>
+          ),
+          dataIndex: "user",
+          align: "center",
+        },
+        {
+          title: (
+            <Title level={4} style={{ marginBottom: 0 }}>
+              Detalles
+            </Title>
+          ),
+          dataIndex: "details",
+          align: "center",
+          render: (text, record) => (
+            <Button type="link" onClick={() => this.showModal(record.key)}>
+              Ver más información
+            </Button>
+          ),
+        },
       ],
       data: [],
+      page: 1,
+      total_users: 0,
+      loading: true,
     };
   }
 
   componentDidMount() {
-    Backend.SendRequest("users", "GET", {}).then(async (response) => {
-      if (response.status === 200) {
-        let res = await response.json();
-        let auxdata = [];
-        res.data.forEach((user) => {
-          auxdata.push({
-            key: user.id,
-            user: user.name,
+    this.makeUsersQuery(1);
+  }
+
+  makeUsersQuery(page) {
+    Backend.SendRequest("users?page=".concat(page), "GET", {}).then(
+      async (response) => {
+        if (response.status === 200) {
+          let res = await response.json();
+          let users_array = [];
+          res.data.forEach((user) => {
+            users_array.push({
+              key: user.id,
+              user: user.name,
+            });
           });
-        });
-        this.setState({
-          data: auxdata,
-        });
-      } else {
-        console.error("Error making the request: " + response.status);
+
+          let total_users = res.meta.pagination.total;
+
+          if (page > 1) {
+            let pre = new Array((page - 1) * 20);
+            pre.fill({});
+            users_array = pre.concat(users_array);
+          }
+
+          if (total_users !== users_array.length) {
+            let pos = new Array(total_users - users_array.length);
+            pos.fill({});
+            users_array = users_array.concat(pos);
+          }
+
+          this.setState({
+            data: users_array,
+            total_users: total_users,
+            loading: false,
+          });
+        } else {
+          console.error("Error making the request: " + response.status);
+        }
       }
-    });
+    );
+  }
+
+  pagChange = (currentPage) => {
+    this.setState(
+      {
+        loading: true,
+      },
+      () => this.makeUsersQuery(currentPage)
+    );
+  };
+
+  showModal(id) {
+    console.log(id);
   }
 
   render() {
@@ -50,8 +110,17 @@ class App extends React.Component {
           <Col xs={22} md={18} lg={12}>
             <Table
               bordered
+              size="small"
               columns={this.state.columns}
               dataSource={this.state.data}
+              loading={this.state.loading}
+              pagination={{
+                defaultPageSize: 20,
+                position: ["topRight"],
+                size: "big",
+                showSizeChanger: false,
+                onChange: this.pagChange,
+              }}
             ></Table>
           </Col>
         </Row>
