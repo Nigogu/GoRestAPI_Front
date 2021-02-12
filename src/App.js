bf08e469ee5d47f0aeefb0beb6f27d7fec0fa50d
Nicolás Gómez <nigomezgu@unal.edu.co>
2021-02-12 12:58:24 -0500

    Creating and using SendRequest.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Backend from "./Backend";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    Backend.SendRequest("users", "GET", {}).then(async (response) => {
+      let res = await response.json();
+      console.log(res);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>Hi.</p>
+      </div>
+    );
+  }
 }
 
 export default App;
